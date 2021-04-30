5d4e56afc4651f5913a22e106d58ea2d2086d05a
zach.gharst <zdgharst@gmail.com>
2021-04-30 12:45:35 -0500

    Normalize data

--- src/main.py
+++ src/main.py
@@ -7,6 +7,14 @@ def one_hot_encode(data, feature):
     result = result.drop([feature], axis = 1)
     return(result)
 
+# https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
+def normalize(data, feature):
+    result = data.copy()
+    max_value = data[feature].max()
+    min_value = data[feature].min()
+    if(max_value != min_value):
+        result[feature] = (data[feature] - min_value) / (max_value - min_value)
+    return(result)
 
 dataset = pd.read_csv("~/Projects/JobChange/input/raw.csv")
 dataset.fillna(0, inplace=True)
@@ -15,5 +23,8 @@ features_to_encode = ['gender', 'relevent_experience', 'enrolled_university', 'e
 'major_discipline', 'company_size', 'company_type', 'last_new_job']
 for feature in features_to_encode:
     dataset = one_hot_encode(dataset, feature)
+
+dataset = normalize(dataset, "training_hours")
+dataset = normalize(dataset, "experience")
 
 print(dataset.head(5))
